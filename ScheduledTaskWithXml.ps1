9fa897f8bb8170fe9d54f9ba3435c3385039dfe2
akchhay <akchhay.alok91@gmail.com>
2020-01-26 12:38:19 +0530

    Add files via upload

---
+++ ScheduledTaskWithXml.ps1
@@ -0,0 +1,7 @@
+﻿# Create scheduled task by importing xml file
+$taskname = "Moving Optio"
+$User = "NT AUTHORITY\SYSTEM" 
+$xmlPath = "C:\ProgramData\Optio\OECI\8.0\spibin\Move_Optio_Files.xml"
+$content =  Get-content $xmlPath |Out-String
+
+Register-ScheduledTask -TaskName $taskname -User $User -Xml $content
