85f9dfc2a79a96beb27630de4e09bc7af00126af
akchhay <akchhay.alok91@gmail.com>
2020-01-26 12:48:39 +0530

    Add files via upload

---
+++ FlagCheck.ps1
@@ -0,0 +1,8 @@
+﻿param(
+[Parameter(Mandatory=$true)][string]$nodeName,
+[Parameter(Mandatory=$true)][string]$xmlPath
+)
+$xml=[xml]( Get-Content $xmlPath )
+$node = $xml.InstallStatus.Methods.$nodeName | where {$_.isError -eq 'False'}
+    $node.isError='True'
+    $xml.save($xmlPath)
